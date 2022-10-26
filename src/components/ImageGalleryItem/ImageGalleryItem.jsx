6dc5b442e23ddef1eece76f1a5f6f36d79cfb90c
Maksi16 <maksim.moroz.kr@gmail.com>
2022-10-26 03:37:04 +0300

    update modal, imggalleryitem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,39 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from '../Modal/Modal';
 import { Image, Item } from './ImageGalleryItem.style';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
-  };
+export function ImageGalleryItem({ smallURL, alt, largeURL }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  toggleModal = () => {
-    this.setState(state => ({ isModalOpen: !state.isModalOpen }));
-  };
-
-  render() {
-    const { isModalOpen } = this.state;
-    const { smallURL, alt, largeURL } = this.props;
-    return (
-      <Item>
-        <Image
-          src={smallURL}
-          alt={alt}
-          width="200"
-          onClick={this.toggleModal}
-        />
-        {isModalOpen && (
-          <Modal
-            closeFn={this.toggleModal}
-            largeImageURL={largeURL}
-            alt={alt}
-          />
-        )}
-      </Item>
-    );
-  }
+  return (
+    <Item>
+      <Image src={smallURL} alt={alt} width="200" onClick={setIsModalOpen} />
+      {isModalOpen && (
+        <Modal closeFn={setIsModalOpen} largeImageURL={largeURL} alt={alt} />
+      )}
+    </Item>
+  );
 }
+
 ImageGalleryItem.propTypes = {
   smallURL: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,33 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay, Modale } from './Modal.style';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+export function Modal({ closeFn, largeImageURL, alt }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  });
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.closeFn();
+      closeFn();
     }
   };
-  handleBackdrope = e => {
+  const handleBackdrope = e => {
     if (e.currentTarget === e.target) {
-      this.props.closeFn();
+      closeFn();
     }
   };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  render() {
-    const { largeImageURL, alt } = this.props;
-    return createPortal(
-      <Overlay onClick={this.handleBackdrope}>
-        <Modale>
-          <img src={largeImageURL} alt={alt} />
-        </Modale>
-      </Overlay>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <Overlay onClick={handleBackdrope}>
+      <Modale>
+        <img src={largeImageURL} alt={alt} />
+      </Modale>
+    </Overlay>,
+    modalRoot
+  );
 }
 
 Modal.propTypes = {
